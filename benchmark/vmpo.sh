15cdb349e740b9756ee2971e3b237965811340ad
Adrian <adrian.nenu@postgrad.manchester.ac.uk>
2026-02-11 21:42:05 +0000

    scripts

--- benchmark/vmpo.sh
+++ benchmark/vmpo.sh
@@ -1,20 +1,9 @@
 # export WANDB_ENTITY=openrlbenchmark
 
-uv pip install .
-OMP_NUM_THREADS=1 xvfb-run -a uv run python -m cleanrl_utils.benchmark \
-    --env-ids CartPole-v1 Acrobot-v1 MountainCar-v0 \
-    --command "uv run python cleanrl/ppo.py --no_cuda --track --capture_video" \
-    --num-seeds 3 \
-    --workers 9 \
-    --slurm-gpus-per-task 1 \
-    --slurm-ntasks 1 \
-    --slurm-total-cpus 10 \
-    --slurm-template-path benchmark/cleanrl_1gpu.slurm_template
-
 uv pip install ".[mujoco]"
 OMP_NUM_THREADS=1 xvfb-run -a python -m cleanrl_utils.benchmark \
     --env-ids HalfCheetah-v4 Walker2d-v4 Hopper-v4 InvertedPendulum-v4 Humanoid-v4 Pusher-v4 \
-    --command "uv run python cleanrl/ppo_continuous_action.py --no_cuda --track --capture_video" \
+    --command "uv run python cleanrl/vmpo_continuous_action.py --no_cuda --track --capture_video" \
     --num-seeds 3 \
     --workers 9 \
     --slurm-gpus-per-task 1 \
@@ -25,19 +14,8 @@ OMP_NUM_THREADS=1 xvfb-run -a python -m cleanrl_utils.benchmark \
 uv pip install ".[mujoco, dm_control]"
 OMP_NUM_THREADS=1 xvfb-run -a uv run python -m cleanrl_utils.benchmark \
     --env-ids dm_control/acrobot-swingup-v0 dm_control/acrobot-swingup_sparse-v0 dm_control/ball_in_cup-catch-v0 dm_control/cartpole-balance-v0 dm_control/cartpole-balance_sparse-v0 dm_control/cartpole-swingup-v0 dm_control/cartpole-swingup_sparse-v0 dm_control/cartpole-two_poles-v0 dm_control/cartpole-three_poles-v0 dm_control/cheetah-run-v0 dm_control/dog-stand-v0 dm_control/dog-walk-v0 dm_control/dog-trot-v0 dm_control/dog-run-v0 dm_control/dog-fetch-v0 dm_control/finger-spin-v0 dm_control/finger-turn_easy-v0 dm_control/finger-turn_hard-v0 dm_control/fish-upright-v0 dm_control/fish-swim-v0 dm_control/hopper-stand-v0 dm_control/hopper-hop-v0 dm_control/humanoid-stand-v0 dm_control/humanoid-walk-v0 dm_control/humanoid-run-v0 dm_control/humanoid-run_pure_state-v0 dm_control/humanoid_CMU-stand-v0 dm_control/humanoid_CMU-run-v0 dm_control/lqr-lqr_2_1-v0 dm_control/lqr-lqr_6_2-v0 dm_control/manipulator-bring_ball-v0 dm_control/manipulator-bring_peg-v0 dm_control/manipulator-insert_ball-v0 dm_control/manipulator-insert_peg-v0 dm_control/pendulum-swingup-v0 dm_control/point_mass-easy-v0 dm_control/point_mass-hard-v0 dm_control/quadruped-walk-v0 dm_control/quadruped-run-v0 dm_control/quadruped-escape-v0 dm_control/quadruped-fetch-v0 dm_control/reacher-easy-v0 dm_control/reacher-hard-v0 dm_control/stacker-stack_2-v0 dm_control/stacker-stack_4-v0 dm_control/swimmer-swimmer6-v0 dm_control/swimmer-swimmer15-v0 dm_control/walker-stand-v0 dm_control/walker-walk-v0 dm_control/walker-run-v0 \
-    --command "uv run python cleanrl/ppo_continuous_action.py --exp-name ppo_continuous_action_8M  --total-timesteps 8000000 --no_cuda --track" \
+    --command "uv run python cleanrl/vmpo_continuous_action.py --exp-name vmpo_continuous_action_8M  --total-timesteps 8000000 --no_cuda --track" \
     --num-seeds 10 \
-    --workers 9 \
-    --slurm-gpus-per-task 1 \
-    --slurm-ntasks 1 \
-    --slurm-total-cpus 10 \
-    --slurm-template-path benchmark/cleanrl_1gpu.slurm_template
-
-uv pip install ".[envpool]"
-uv run python -m cleanrl_utils.benchmark \
-    --env-ids Pong-v5 BeamRider-v5 Breakout-v5 \
-    --command "uv run python cleanrl/ppo_atari_envpool.py --track --capture_video" \
-    --num-seeds 3 \
     --workers 9 \
     --slurm-gpus-per-task 1 \
     --slurm-ntasks 1 \
