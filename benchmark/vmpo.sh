c52196a8b8f4b681ce09e3532e5e4b750ab9b8a5
Adrian <adrian.nenu@postgrad.manchester.ac.uk>
2026-02-12 13:48:28 +0000

    bench

--- benchmark/vmpo.sh
+++ benchmark/vmpo.sh
@@ -3,13 +3,53 @@
 uv pip install ".[mujoco]"
 uv run python -m cleanrl_utils.benchmark \
     --env-ids HalfCheetah-v4 Walker2d-v4 Hopper-v4 Humanoid-v4 Pusher-v4 \
-    --command "uv run python cleanrl/vmpo_continuous_action.py --track --capture_video" \
+    --command "uv run python cleanrl/vmpo_continuous_action.py --track --capture_video --wandb_project_name cleanrl-bench-1" \
     --num-seeds 1 \
     --workers 2
 
 uv pip install ".[mujoco, dm_control]"
 uv run python -m cleanrl_utils.benchmark \
     --env-ids dm_control/cartpole-balance-v0 dm_control/cheetah-run-v0 dm_control/dog-walk-v0 dm_control/dog-run-v0 dm_control/hopper-hop-v0 dm_control/humanoid-walk-v0 dm_control/humanoid-run-v0 dm_control/humanoid-run_pure_state-v0 dm_control/humanoid_CMU-run-v0 dm_control/walker-walk-v0 dm_control/walker-run-v0 \
-    --command "uv run python cleanrl/vmpo_continuous_action.py --track" \
+    --command "uv run python cleanrl/vmpo_continuous_action.py --track --wandb_project_name cleanrl-bench-1" \
     --num-seeds 1 \
     --workers 2
+
+
+uv run python -m cleanrl_utils.benchmark \
+    --env-ids HalfCheetah-v4 Walker2d-v4 Hopper-v4 Humanoid-v4 Pusher-v4 \
+    --command "uv run python cleanrl/vmpo_continuous_action.py --track --capture_video --wandb_project_name cleanrl-bench-2-dae" \
+    --num-seeds 1 \
+    --workers 2 --advantage_estimator dae
+
+uv pip install ".[mujoco, dm_control]"
+uv run python -m cleanrl_utils.benchmark \
+    --env-ids dm_control/cartpole-balance-v0 dm_control/cheetah-run-v0 dm_control/dog-walk-v0 dm_control/dog-run-v0 dm_control/hopper-hop-v0 dm_control/humanoid-walk-v0 dm_control/humanoid-run-v0 dm_control/humanoid-run_pure_state-v0 dm_control/humanoid_CMU-run-v0 dm_control/walker-walk-v0 dm_control/walker-run-v0 \
+    --command "uv run python cleanrl/vmpo_continuous_action.py --track --wandb_project_name cleanrl-bench-2-dae" \
+    --num-seeds 1 \
+    --workers 2 --advantage_estimator dae
+
+uv run python -m cleanrl_utils.benchmark \
+    --env-ids HalfCheetah-v4 Walker2d-v4 Hopper-v4 Humanoid-v4 Pusher-v4 \
+    --command "uv run python cleanrl/vmpo_continuous_action.py --track --capture_video --wandb_project_name cleanrl-bench-2-gae" \
+    --num-seeds 1 \
+    --workers 2 --advantage_estimator gae
+
+uv pip install ".[mujoco, dm_control]"
+uv run python -m cleanrl_utils.benchmark \
+    --env-ids dm_control/cartpole-balance-v0 dm_control/cheetah-run-v0 dm_control/dog-walk-v0 dm_control/dog-run-v0 dm_control/hopper-hop-v0 dm_control/humanoid-walk-v0 dm_control/humanoid-run-v0 dm_control/humanoid-run_pure_state-v0 dm_control/humanoid_CMU-run-v0 dm_control/walker-walk-v0 dm_control/walker-run-v0 \
+    --command "uv run python cleanrl/vmpo_continuous_action.py --track --wandb_project_name cleanrl-bench-2-gae" \
+    --num-seeds 1 \
+    --workers 2 --advantage_estimator gae
+
+uv run python -m cleanrl_utils.benchmark \
+    --env-ids HalfCheetah-v4 Walker2d-v4 Hopper-v4 Humanoid-v4 Pusher-v4 \
+    --command "uv run python cleanrl/vmpo_continuous_action.py --track --capture_video --wandb_project_name cleanrl-bench-2-sgd" \
+    --num-seeds 1 \
+    --workers 2 --optimizer sgd
+
+uv pip install ".[mujoco, dm_control]"
+uv run python -m cleanrl_utils.benchmark \
+    --env-ids dm_control/cartpole-balance-v0 dm_control/cheetah-run-v0 dm_control/dog-walk-v0 dm_control/dog-run-v0 dm_control/hopper-hop-v0 dm_control/humanoid-walk-v0 dm_control/humanoid-run-v0 dm_control/humanoid-run_pure_state-v0 dm_control/humanoid_CMU-run-v0 dm_control/walker-walk-v0 dm_control/walker-run-v0 \
+    --command "uv run python cleanrl/vmpo_continuous_action.py --track --wandb_project_name cleanrl-bench-2-sgd" \
+    --num-seeds 1 \
+    --workers 2 --optimizer sgd
